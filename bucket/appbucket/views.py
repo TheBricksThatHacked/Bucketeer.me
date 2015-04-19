557d938e22115240349b7f3ec0d1a758a8e83b7f
Zachary Friss <friss@frissdesigns.com>
2015-04-19 00:12:55 -0400

    Hook up the item completion stats

--- bucket/appbucket/views.py
+++ bucket/appbucket/views.py
@@ -12,7 +12,14 @@ def user_profile(request, user_id=None):
     user = User.objects.get(id=user_id)
     enc_email = user.email.strip().lower().encode("utf-8")
     email_hash = hashlib.md5(enc_email).hexdigest()
-    return render_to_response("profile.html", {'userProfile': user, 'email_hash': email_hash}, context_instance=RequestContext(request))
+    user_items = Item.objects.filter(user = user).order_by('-completed_date')
+    items_completed = Item.objects.filter(user = user).exclude(completed_date__isnull=True)
+    numTotal = len(user_items)
+    if numTotal == 0:
+      numTotal = 1
+    percentCompleted = (len(items_completed) / numTotal ) * 100
+    percentUnCompleted = 100 -percentCompleted
+    return render_to_response("profile.html", {'percentUnCompleted': percentUnCompleted,'percentCompleted': percentCompleted, 'userProfile': user, 'email_hash': email_hash, 'user_items': user_items, 'items_completed': items_completed}, context_instance=RequestContext(request))
 
 def my_profile(request):
     return user_profile(request, user_id=request.user.id)
