59d3ddf2000038a4ceb34172b6ee5cd6cb18b332
Adam McCarthy <aem1269@rit.edu>
2015-04-19 02:55:44 -0400

    Add ajax checkoff function

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,19 @@
-
+function checkOff(id) {
+    data = {};
+    data['csrfmiddlewaretoken'] = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+    data['item-id'] = id;
+    $.ajax({
+        url: '/ajax/checkoff/',
+        dataType: 'json',
+        method: 'POST',
+        data: data,
+        success: function(response) {
+            if (response.checked) {
+                // TODO check off the thing
+            }
+        }, error: function(jqXHR) {
+            console.log("An ajax error occurred");
+            console.log(jqXHR);
+        }
+    });
+}
