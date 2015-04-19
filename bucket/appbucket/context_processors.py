57c8aeb772dc7a3a99145999847e182c82f188cf
corbanmailloux <cdm3806@rit.edu>
2015-04-19 09:39:27 -0400

    Cleanups.

--- bucket/appbucket/context_processors.py
+++ bucket/appbucket/context_processors.py
@@ -2,4 +2,4 @@ from django.contrib.auth.forms import AuthenticationForm
 
 def include_login_form(request):
     form = AuthenticationForm()
-    return {'login_form': form, 'test': "TEST"}
+    return {'login_form': form}
