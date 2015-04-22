9f3c1e5323fea3d01fa6b6689b899ef1d8e069ee
corbanmailloux <cdm3806@rit.edu>
2015-04-22 19:58:04 -0400

    Playing with private items.

--- bucket/appbucket/models.py
+++ bucket/appbucket/models.py
@@ -11,6 +11,7 @@ class Item(models.Model):
     completed_date = models.DateTimeField(blank=True, null=True)
     user = models.ForeignKey(User)
     tags = TaggableManager(blank=True)
+    private = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
