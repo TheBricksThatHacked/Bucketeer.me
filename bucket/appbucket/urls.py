c26520ca4aeda48ff23ebd0683af7800dfea6f1b
corbanmailloux <cdm3806@rit.edu>
2015-05-16 13:13:52 -0400

    Update URL regex to match user name regex.

--- bucket/appbucket/urls.py
+++ bucket/appbucket/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     url(r'^profile/$', 'appbucket.views.my_profile', name='my_profile'),
     url(r'^profile/id/(?P<user_id>\d+)/$', 'appbucket.views.user_profile', name='user_profile'),
     url(r'^profile/edit/$', 'appbucket.views.edit_profile', name='edit_profile'),
-    url(r'^profile/(?P<user_name>[\w.@+-]+)/$', 'appbucket.views.user_profile', name='user_profile_name'),
+    url(r'^profile/(?P<user_name>[a-zA-Z0-9.-]+)/$', 'appbucket.views.user_profile', name='user_profile_name'),
     url(r'^addItem/$', 'appbucket.views.add_item', name='add_item'),
     url(r'^item/edit/(?P<item_id>\d+)/$', 'appbucket.views.edit_item', name='edit_item'),
     
